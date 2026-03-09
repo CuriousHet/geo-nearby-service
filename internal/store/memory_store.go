ea309c553a5a04d9bb5b58e52c98420320b3a54c
CuriousHet <het18211@gmail.com>
2026-03-09 11:45:02 +0530

    addded more dummy data co-ordinates

--- internal/store/memory_store.go
+++ internal/store/memory_store.go
@@ -1,4 +1,5 @@
-package store 
+package store
+
 import (
 	"github.com/CuriousHet/geo-nearby-service/internal/models"
 )
@@ -13,6 +14,28 @@ func NewMemoryStore() *MemoryStore {
 		{ID: 1, Latitude: 23.0225, Longitude: 72.5714},
 		{ID: 2, Latitude: 23.0300, Longitude: 72.5800},
 		{ID: 3, Latitude: 19.0760, Longitude: 72.8777},
+		{ID: 4, Latitude: 28.7041, Longitude: 77.1025},
+		{ID: 5, Latitude: 12.9716, Longitude: 77.5946},
+		{ID: 6, Latitude: 13.0827, Longitude: 80.2707},
+		{ID: 7, Latitude: 22.5726, Longitude: 88.3639},
+		{ID: 8, Latitude: 17.3850, Longitude: 78.4867},
+		{ID: 9, Latitude: 26.9124, Longitude: 75.7873},
+		{ID: 10, Latitude: 18.5204, Longitude: 73.8567},
+		{ID: 11, Latitude: 23.2599, Longitude: 77.4126},
+		{ID: 12, Latitude: 21.1702, Longitude: 72.8311},
+		{ID: 13, Latitude: 22.3072, Longitude: 73.1812},
+		{ID: 14, Latitude: 26.8467, Longitude: 80.9462},
+		{ID: 15, Latitude: 25.3176, Longitude: 82.9739},
+		{ID: 16, Latitude: 24.5854, Longitude: 73.7125},
+		{ID: 17, Latitude: 30.7333, Longitude: 76.7794},
+		{ID: 18, Latitude: 15.2993, Longitude: 74.1240},
+		{ID: 19, Latitude: 11.0168, Longitude: 76.9558},
+		{ID: 20, Latitude: 9.9312, Longitude: 76.2673},
+		{ID: 21, Latitude: 16.5062, Longitude: 80.6480},
+		{ID: 22, Latitude: 23.3441, Longitude: 85.3096},
+		{ID: 23, Latitude: 26.1445, Longitude: 91.7362},
+		{ID: 24, Latitude: 27.1767, Longitude: 78.0081},
+		{ID: 25, Latitude: 29.9457, Longitude: 78.1642},
 	}
 
 	return &MemoryStore{
